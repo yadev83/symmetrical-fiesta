edfaae9aa1f57b2a441c0242b056284d11b14ce5
Yanis <yanis.attia@outlook.fr>
2024-03-19 23:49:10 +0100

    adds most dependancies + initial server setup

---
+++ src/server/index.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 80
+
+const publicPath = path.resolve(__dirname, '../../public')
+const viewsPath = path.resolve(__dirname, '../application/views')
+
+app.use(express.static(publicPath))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(viewsPath, 'index.html'))
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+})
+
+app.listen(port, () => {
+    console.log(`SERVER Listening : http://localhost:${port}`)
+})
